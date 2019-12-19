17f7a7cf4e2fb97a444835597d0575eef34037fd
Ahmed Osama <eng.ahmed.osama.1190@gmail.com>
2019-12-19 17:58:30 +0200

    first commit

---
+++ assets/js/plugin.js
@@ -0,0 +1,27 @@
+WebChat.default.init({
+    selector: "#webchat",
+    initPayload: "/get_started",
+    interval: 100, // 1000 ms between each message
+    customData: {"userId": "333", "language": "ar"}, // arbitrary custom data. Stay minimal as this will be added to the socket
+    socketUrl: "http://167.172.226.61:5005",
+	//socketUrl: "http://localhost:5050",
+    socketPath: "/socket.io/",
+    title: "FCA Chatbot",
+    subtitle: "typically replies instantly",
+	session_persistence: true,
+    inputTextFieldHint: "Type a message...",
+    connectingText: "Waiting for server...",
+    hideWhenNotConnected: true,
+    fullScreenMode: false,
+    profileAvatar: "Avatar.png",
+
+    params: {
+      images: {
+        dims: {
+          width: 300,
+          height: 200,
+        }
+      },
+      storage: "local"
+    }
+  })
